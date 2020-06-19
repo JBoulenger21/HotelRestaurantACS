3bb3875c7a73bff63dace60cab66033e1c1cb5f4
pauline-mfr <p.monfrance@codeur.online>
2020-06-19 09:06:29 +0200

    sauvergarde av modif section 1

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aux Truites Bleues</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+
+<!-- SECTION A PROPOS -->
+
+<section id="bienvenue">
+  <h3>Bienvenue aux truites bleues</h3>
+  <div class="underline"></div>
+  <div id="container-bienvenue">
+  <img src="propos1.jpg" alt="piscine">
+  <div id="block2">
+    <img src="propos2.jpg" alt="vélo">
+  <img src="propos3.jpg" alt="lac" id="propos3">
+</div>
+  <div id="propos">
+  <h2>à <span>propos</span></h2>
+  <p>Aux portes du Parc Naturel du Haut Jura idéalement situé entre Champagnole et Saint Laurent en Grandvaux non loin de la station des Rousses, notre établissement Aux Truites Bleus vous accueille tout au long de l’année et saura vous faire découvrir les trésors de la Nature Jurassienne.</p>
+  <button>en savoir plus</button>
+</div>
+</div>
+</section>
+<div class="underline"></div>
+
+<!-- SECTION PRESTATIONS -->
+
+<section id="prestations">
+
+  <div id="hotel">
+  <img src="hotel.jpg" alt="réception">
+  <div id="tx-hotel">
+<h3>Notre hôtel</h3>
+<div class="underline"></div>
+  <p>Notre Hôtel de charme dans le jura vous accueille pour vos séjours vacances :
+ Chambres doubles, suite parentales, petits déjeuners, service de restauration…
+Situé dans le cadre somptueux de la région dtes lacs du Jura, entouré de verdure et
+au bord d’une rivière, notre hôtel placé aux portes du Parc Naturel du Haut Jura
+saura vous offrir tous les ingrédients d’un séjour nature réussi, qu’il soit en famille,
+entre amis ou même à deux !</p>
+<button>Découvrir</button>
+</div>
+</div>
+
+<div id="resto">
+  <div id="tx-resto">
+  <h3>Notre restaurant</h3>
+  <div class="underline"></div>
+  <p>Au restaurant des Truites Bleues, les plaisirs de la table
+sont à la fois spectacle et gourmandise !
+Dans notre restaurant,vin jaune, vin d’Arbois et autres délices du pays
+sont les vedettes d’une cuisine de caractère.
+Découvrez la gastronomie et les produits typiques de la région du Jura !</p>
+<button>Découvrir</button>
+</div>
+<img src="resto.jpg" alt="restaurant">
+</div>
+
+<div id="services">
+  <img src="services.jpg" alt="réception">
+  <div id="tx-services">
+  <h3>Nos services</h3>
+  <div class="underline"></div>
+  <p>Nous sommes là pour vous !
+Notre équipe vous propose ses services pour vos grandes occasions :
+anniversaire, mariage, communion, déjeuner d’entreprise.
+C’est avec un réel plaisir que notre équipe
+vous accompagnera dans votre projet suivant vos envies et votre budget.</p>
+<button>Découvrir</button>
+</div>
+</div>
+
+<div id="sejours">
+  <div id="tx-sejours">
+  <h3>Nos séjours</h3>
+  <div class="underline"></div>
+  <p>Venez vous ressourcer dans le Haut Jura et partez à
+la découverte des trésors de la nature en toute simplicité.
+Des séjours conçus selon vos envie, vos attentes et votre budget.
+Une escapade d’une nuit ou plus à vous de choisir !
+Notre Hôtel*** authentique vous accueille tout
+au long de l’année et au fil des saisons.
+Une multitude d’activités s’offrent à vous …</p>
+<button>Découvrir</button>
+</div>
+<img src="sejours.jpg" alt="lac">
+</div>
+
+</section>
+
+<!-- SECTION ACTU -->
+
+<section id="actu">
+  <h2>Nos <span>actualités</span></h2>
+  <div class="underline"></div>
+<div id="row-actu">
+  <div id="actu1">
+    <img src="actu1.jpg" alt="buffet">
+    20 décembre
+    Fêtes de fin d'année ?
+    Partagez un bon repas !
+    <img src="com_icon.png" alt="icone commentaire">
+    5 commentaires
+    <p>Cette période de fin d'année est propice au moment de partage.
+Alors pourquoi ne pas s'offrir un déjeuner entre amis, collègues... ?
+ A vous les pros, qui souhaitez remercier vos collaborateurs, vos
+partenaires, pensez au repas de fin d'année.</p>
+Lire plus <img src="arrow_icon.png" alt="flèche">
+  </div>
+
+  <div id="actu2">
+    <img src="actu2.jpg" alt="menu restaurant">
+    30 décembre
+    Restaurant : la carte du moment
+    <img src="com_icon.png" alt="icone commentaire">
+    7 commentaires
+    <p>Notre carte hivernale en fonction de vous. De longues soirées
+d'hiver nous attendent... Laissez - vous séduire par un bon repas
+auprès de la cheminée de notre restaurant. Dans une ambiance
+chaleureuse et douce c'est avec plaisir que notre chef vous propose
+ une cuisine conviviale dans sa nouvelle carte hivernale.</p>
+Lire plus <img src="arrow_icon.png" alt="flèche">
+  </div>
+
+  <div id="actu3">
+    <img src="actu3.jpg" alt="couloir">
+    10 mars
+    Les infos pratiques :
+    Horaires, contact, actus
+    <img src="com_icon.png" alt="icone commentaire">
+    2 commentaires
+    <p>Horaires, infos, actus, promo</p>
+Lire plus <img src="arrow_icon.png" alt="flèche">
+  </div>
+</div>
+
+<!-- insérer les dots -->
+
+</section>
+
+<!-- SECTION AVIS -->
+
+<section id="avis">
+  <h3>Avis clients</h3>
+  <img src="opening_quote.png" alt="opening quote">
+  <p>Un établissement agréable avec un hôtel de bon confort
+et un restaurant avec de bons produits locaux.
+Une salle à manger chaleureuse avec sa cheminée
+centrale et un service professionnel et souriant.</p>
+<img src="closing_quote.png" alt="closing quote">
+<p>____ Pascal.P, le 27/02/2020</p>
+90%
+</section>
+
+  </body>
+</html>
