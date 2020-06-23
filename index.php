460c6212cda5a388c9689650935ecbd055be42c5
pauline-mfr <p.monfrance@codeur.online>
2020-06-23 16:48:11 +0200

    version mobile

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
     <title>Aux Truites Bleues</title>
     <link rel="stylesheet" type="text/css" href="main.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
-  <body>
+  <body id="page-accueil">
 
 <!-- SECTION A PROPOS -->
 
@@ -15,15 +16,16 @@
   <div id="container-bienvenue">
   <h3>Bienvenue aux truites bleues</h3>
   <div class="underline"></div>
-  <img src="propos.jpg" alt="hotel jura">
+  <img src="propos.jpg" alt="hotel jura" id="img_desk">
+  <img src="propos_mob.jpg" alt="hotel jura" id="img_mob">
   <div id="tx-propos">
   <h2>à <span>propos</span></h2>
-  <p>Aux portes du Parc Naturel du Haut Jura idéalement<br>situé entre Champagnole et Saint Laurent en<br>Grandvaux non loin de la station des Rousses,<br>notre établissement Aux Truites Bleus vous<br>accueille tout au long de l’année et saura vous<br>faire découvrir les trésors de la Nature Jurassienne.</p>
+  <p>Aux portes du Parc Naturel du Haut Jura idéalement<br>situé entre Champagnole et Saint Laurent en<br>Grandvaux non loin de la <em>station des Rousses,<br>notre établissement Aux Truites Bleus vous<br>accueille tout au long de l’année et saura vous<br>faire découvrir les trésors de la Nature Jurassienne.</em></p>
   <button>en savoir plus</button>
 </div>
 </div>
 </section>
-<div class="underline"></div>
+<div class="underline" id="extra_under"></div>
 
 <!-- SECTION PRESTATIONS -->
 
@@ -32,41 +34,43 @@
   <div id="hotel">
   <img src="hotel.jpg" alt="réception">
   <div id="tx-hotel">
-<h3>Notre hôtel</h3>
-<div class="underline"></div>
+    <h3>Notre hôtel</h3>
+    <div class="underline_desk"></div>
   <p>Notre Hôtel de charme dans le jura vous accueille pour vos séjours vacances :<br>Chambres doubles, suite parentales, petits déjeuners, service de restauration…<br>Situé dans le cadre somptueux de la région dtes lacs du Jura, entouré de verdure et<br>au bord d’une rivière, notre hôtel placé aux portes du Parc Naturel du Haut Jura<br>saura vous offrir tous les ingrédients d’un séjour nature réussi, qu’il soit en famille,<br>entre amis ou même à deux !</p>
 <button>Découvrir</button>
 </div>
 </div>
 
 <div id="resto">
+  <img src="resto.jpg" alt="restaurant" id="resto_mob">
   <div id="tx-resto">
   <h3>Notre restaurant</h3>
-  <div class="underline"></div>
-  <p>Au restaurant des Truites Bleues, les plaisirs de la table<br>sont à la fois spectacle et gourmandise !<br>Dans notre restaurant,vin jaune, vin d’Arbois et autres délices du pays<br>sont les vedettes d’une cuisine de caractère.<br>Découvrez la gastronomie et les produits typiques de la région du Jura !</p>
+  <div class="underline_desk"></div>
+  <p>Au restaurant des Truites Bleues, les plaisirs de la table <br>sont à la fois spectacle et gourmandise ! <br>Dans notre restaurant,vin jaune, vin d’Arbois et autres délices du pays <br>sont les vedettes d’une cuisine de caractère. <br>Découvrez la gastronomie et les produits typiques de la région du Jura !</p>
 <button>Découvrir</button>
 </div>
-<img src="resto.jpg" alt="restaurant">
+<img src="resto.jpg" alt="restaurant" id="resto_desk">
 </div>
 
 <div id="services">
   <img src="services.jpg" alt="réception">
   <div id="tx-services">
-  <h3>Nos services</h3>
-  <div class="underline"></div>
-  <p>Nous sommes là pour vous !<br>Notre équipe vous propose ses services pour vos grandes occasions :<br>anniversaire, mariage, communion, déjeuner d’entreprise.<br><br>C’est avec un réel plaisir que notre équipe<br>vous accompagnera dans votre projet suivant vos envies et votre budget.</p>
+    <h3>Nos services</h3>
+    <div class="underline_desk"></div>
+  <p>Nous sommes là pour vous ! <br>Notre équipe vous propose ses services pour vos grandes occasions : <br>anniversaire, mariage, communion, déjeuner d’entreprise.<br><br>C’est avec un réel plaisir que notre équipe <br>vous accompagnera dans votre projet suivant vos envies et votre budget.</p>
 <button>Découvrir</button>
 </div>
 </div>
 
 <div id="sejours">
+  <img src="sejours.jpg" alt="lac" id="sejours_mob">
   <div id="tx-sejours">
   <h3>Nos séjours</h3>
-  <div class="underline"></div>
-  <p>Venez vous ressourcer dans le Haut Jura et partez à<br>la découverte des trésors de la nature en toute simplicité.<br><br>Des séjours conçus selon vos envie, vos attentes et votre budget.<br>Une escapade d’une nuit ou plus à vous de choisir !<br>Notre Hôtel*** authentique vous accueille tout<br>au long de l’année et au fil des saisons.<br>Une multitude d’activités s’offrent à vous …</p>
+  <div class="underline_desk"></div>
+  <p>Venez vous ressourcer dans le Haut Jura et partez à <br>la découverte des trésors de la nature en toute simplicité.<br><br>Des séjours conçus selon vos envie, vos attentes et votre budget. <br>Une escapade d’une nuit ou plus à vous de choisir ! <br>Notre Hôtel*** authentique vous accueille tout <br>au long de l’année et au fil des saisons. <br>Une multitude d’activités s’offrent à vous …</p>
 <button>Découvrir</button>
 </div>
-<img src="sejours.jpg" alt="lac">
+<img src="sejours.jpg" alt="lac" id="sejours_desk">
 </div>
 
 </section>
@@ -86,7 +90,7 @@
     <h4>Fêtes de fin d'année ?<br>Partagez un bon repas !</h4>
   </div>
     <p class="com"><img src="com_icon.png" alt="icone commentaire"> 5 commentaires</p>
-    <p class="para-actu">Cette période de fin d'année est propice au moment de partage.<br>Alors pourquoi ne pas s'offrir un déjeuner entre amis, collègues... ?<br>A vous les pros, qui souhaitez remercier vos collaborateurs, vos<br>partenaires, pensez au repas de fin d'année.</p>
+    <p class="para-actu">Cette période de fin d'année est propice au moment de partage.<br>Alors pourquoi ne pas s'offrir un déjeuner <em>entre amis, collègues... ?<br>A vous les pros, qui souhaitez remercier vos collaborateurs, vos<br>partenaires, pensez au repas de fin d'année.</em></p>
 <a href="#">Lire plus <img src="arrow_icon.png" alt="flèche"></a>
   </div>
 
@@ -116,23 +120,25 @@
 <a href="#">Lire plus <img src="arrow_icon.png" alt="flèche"></a>
   </div>
 </div>
-
-<!-- insérer les dots -->
-
+<div id="nav-actu">
+  <p id="dot1"></p><p id="dot2"></p><p id="dot2"></p>
+</div>
 </section>
 
 <!-- SECTION AVIS -->
 
 <section id="avis">
   <div id="tx-avis">
-  <h3>Avis <span>clients</span></h3>
+    <h3>Avis <span>clients</span></h3>
 <p><img src="opening_quote.png" alt="opening quote"> Un établissement agréable avec un hôtel de bon confort<br>et un restaurant avec de bons produits locaux.<br>Une salle à manger chaleureuse avec sa cheminée<br>centrale et un service professionnel et souriant. <img src="closing_quote.png" alt="closing quote"></p>
-<p>____ <span>Pascal.P, le 27/02/2020</span></p>
+<p id="auteur">____ <span>Pascal.P, le 27/02/2020</span></p>
 </div>
 <div id="pourcentage">
 <p><br>90%</p>
 </div>
-<div id="nav-avis">___ ___ ___</div>
+<div id="nav-avis">
+  <p><span>___</span> ___ ___</p>
+</div>
 </section>
 
   </body>
