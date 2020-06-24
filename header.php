d6729a28637bb3a3ba84355541d755ca80dbad86
MariusPaq <marius.paquet@gmail.com>
2020-06-24 15:07:02 +0200

    header

--- header.php
+++ header.php
@@ -30,3 +30,16 @@
     </div>
 </div>
 <!--FIN HEADER-->
+<script>
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    var y = document.getElementById("contact-infonav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      y.style.display = "block";
+    } else {
+      x.style.display = "block";
+      y.style.display = "none";
+    }
+  }
+</script>
