700dc727b195d329d747ce8b0c9a4e5597e80714
JBoulenger21 <boulenger.julie@gmail.com>
2020-06-25 11:58:35 +0200

    final final

--- navbar.php
+++ navbar.php
@@ -1,22 +1,22 @@
 <!--HEADER-->
 <img class="contact-logo" src="img/Logo.png" alt="">
-<a href="#"><img class="contact-home"src="img/Home.png" alt=""></a>
+<a href="index.php"><img class="contact-home"src="img/Home.png" alt=""></a>
 <div class="contact-navbar">
   <a id="contact-loupe" href="#"><img src="img/Search.png" alt=""></a>
   <a class="contact-colorwhite contact-fontmontserrat contact-bold"href="#">SEARCH</a>
-  <a class="contact-colorwhite contact-fontmontserrat contact-bold"href="#">CONTACT</a>
+  <a class="contact-colorwhite contact-fontmontserrat contact-bold"href="contact.php">CONTACT</a>
   <a class="contact-colorwhite contact-fontmontserrat contact-bold"href="#">SEJOURS</a>
-  <a class="contact-colorwhite contact-fontmontserrat contact-bold"href="#">SERVICES</a>
+  <a class="contact-colorwhite contact-fontmontserrat contact-bold"href="services.php">SERVICES</a>
   <a class="contact-colorwhite contact-fontmontserrat contact-bold"href="#">RESTAURANT</a>
-  <a id="contact-ahotel" class="contact-colorwhite contact-fontmontserrat contact-bold"href="#">HÔTEL</a>
+  <a id="contact-ahotel" class="contact-colorwhite contact-fontmontserrat contact-bold"href="hotel.php">HÔTEL</a>
 </div>
 <div class="contact-navbarMobile">
-  <a href="#home" class="active"></a>
+  <a href="index.php" class="active"></a>
     <div id="myLinks">
       <a class="contact-anavbarmsearch" href="#search">SEARCH</a></br>
-      <a class="contact-anavbarmcontact" href="#contact">CONTACT</a></br>
+      <a class="contact-anavbarmcontact" href="contact.php">CONTACT</a></br>
       <a class="contact-anavbarmsejours" href="#sejours">SEJOURS</a></br>
-      <a class="contact-anavbarmservices" href="#services">SERVICES</a></br>
+      <a class="contact-anavbarmservices" href="services.php">SERVICES</a></br>
       <a class="contact-anavbarmrestaurant" href="#restaurant">RESTAURANT</a></br>
     </div>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
